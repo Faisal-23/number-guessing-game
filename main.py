82836ca81664b48cff4841f6f21c00674e9bba68
Faisal-523 <faisalbukhari523@gmail.com>
2021-11-27 18:17:06 +0530

    Added code to genrate random number

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
+import random
+
 num = int(input("Guess a number between 1 and 10\n"))
 print(f"Your number is {num}")
+
+num2 = random.randint(1,10)
+
+if num == num2:
+    print("You guessed a right number")
+else:
+    print("You guessed a wrong number")
