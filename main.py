fe3f0a51149fdf4d07f5c6597f021c8b3628ac37
Faisal-523 <faisalbukhari523@gmail.com>
2021-11-28 17:57:31 +0530

    Added code for computer guess

--- main.py
+++ main.py
@@ -3,12 +3,39 @@
 
 import random
 
-num = int(input("Guess a number between 1 and 10\n"))
-print(f"Your number is {num}")
+def guess():
+    num2 = random.randint(1,10)
+    num = 0
+    while num2 != num:
+        num = int(input("Guess a number between 1 and 10\n"))
+        print(f"Your number is {num}")
 
-num2 = random.randint(1,10)
+        if num == num2:
+            print("You guessed a right number")
+        elif num < num2:
+            print(f"Number is bigger than {num}")
+        else:
+            print(f"number is less than {num}")
 
-if num == num2:
-    print("You guessed a right number")
-else:
-    print("You guessed a wrong number")
+def comp_guess():
+    num = 0
+    feedback = ''
+    low = 1
+    high = 1000
+    while feedback != 'c':
+        if high != low:
+            num = random.randint(low,high)
+        else:
+            num = low
+        feedback = input(f"Is {num} the number you guessed? high (h), low (l), correct(c)")
+
+        if feedback == 'h':
+            low = num+1
+        elif feedback == 'l':
+            high = num-1
+    
+    print(f"Yayy! The number is {num}")
+
+
+comp_guess()
+guess()
